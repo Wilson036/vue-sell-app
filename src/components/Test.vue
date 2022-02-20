6b0a74af792933a30f4bdfc16b69049c2e6339f8
wilsonkao <wilkao036@gmail.com>
2022-02-21 00:22:15 +0800

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>test test 111</h1>
+    <h2>111{{ msg }}</h2>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "hell test hyh ",
+    };
+  },
+};
+</script>       
